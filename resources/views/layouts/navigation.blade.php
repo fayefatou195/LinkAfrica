66442ed5d6a968b01981e92a880c7355791830d4
fayefatou195 <mbaat195@gmail.com>
2023-05-23 20:12:17 +0200

    second commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -42,7 +42,7 @@
       <div class="hidden sm:flex sm:items-center sm:ml-6">
               <ul class="nav justify-content-end">
               <li class="nav-link">
-                <a class="nav-link " href="links">Voir Plus</a>   
+                <a class="nav-link " href="/">Raccourcir un autre Lien </a>   
                 </li>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
